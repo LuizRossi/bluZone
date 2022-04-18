b67e1904d0a58cde7c9ac05abb6d0cb43f99e143
Luiz Rossi <luiz.rossi@somasolucoes.com>
2022-04-18 20:01:37 -0300

    alterações luiz

--- app/src/main/java/br/com/bluzone/MainActivity.kt
+++ app/src/main/java/br/com/bluzone/MainActivity.kt
@@ -2,19 +2,26 @@ package br.com.bluzone
 
 import android.content.Intent
 import android.net.ConnectivityManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import br.com.bluzone.databinding.ActivityMainBinding
-import br.com.bluzone.databinding.ActivityStatusveiculoBinding
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
+import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var functions: FirebaseFunctions
+
+    // instanciando um objeto gson
+    private val gson = GsonBuilder().enableComplexMapKeySerialization().create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        functions = Firebase.functions("southamerica-east1")
         //Inflar a activity
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -38,5 +45,4 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-}
 }
